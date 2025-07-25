bc6f5adff98676be8bf37ed8c13c6f3af738943d
Katya Weicht <katya.weicht@gmail.com>
2025-07-25 10:51:08 -0600

    Add association proxy's to Employee and Project models

--- server/models.py
+++ server/models.py
@@ -27,6 +27,9 @@ class Employee(db.Model):
     meetings = db.relationship('Meeting', secondary = employee_meetings, back_populates = 'employees')
     assignments = db.relationship('Assignment', back_populates='employee', cascade='all, delete-orphan')
 
+    #association proxy to get projects for this employee through assignments
+    projects = association_proxy(association_proxy('assignments', 'project', creator=lambda project_obj: Assignment(project=project_obj)))
+
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.hire_date}>'
 
@@ -53,6 +56,9 @@ class Project(db.Model):
     budget = db.Column(db.Integer)
 
     assignments = db.relationship('Assignment', back_populates='project',cascade='all, delete-orphan')
+
+    # Association proxy to get employees for this project through assignments
+    employees = association_proxy('assignments', 'employee', creator=lambda employee_obj: Assignment(employee=employee_obj))
 
     def __repr__(self):
         return f'<Review {self.id}, {self.title}, {self.budget}>'
